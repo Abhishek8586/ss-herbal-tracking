d340863d6ee9de222d18729ea873320c7a787ae6
Abhishek8586 <abhishekssherbal18@gmail.com>
2026-07-19 17:02:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("SS Herbal India Tracking System");
+function trackParcel(){
+
+    let awb = document.getElementById("awb").value;
+
+    let result = document.getElementById("result");
+
+    if(awb=="45993612345678"){
+
+        result.innerHTML=`
+        <br>
+        <b>Customer :</b> Rahul Sharma<br><br>
+
+        <b>Courier :</b> Delhivery<br><br>
+
+        <b>Status :</b>
+        <span style="color:green;">In Transit</span>
+        `;
+
+    }else{
+
+        result.innerHTML="<span style='color:red'>AWB Not Found</span>";
+
+    }
+
+}
